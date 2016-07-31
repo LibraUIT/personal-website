98428456ec6f88598f9e19549b1c740095c809c0
Quan Nguyen <quanmn.libra@gmail.com>
2016-07-31 13:07:12 +0700

    Admin Sample Page

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,11 @@ class Admin::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_filter :is_admin!
   layout 'admin'
+
+  def is_admin!
+    redirect_to root_path unless current_user.admin?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum user_type: { admin: 1, user: 2 }
 end
